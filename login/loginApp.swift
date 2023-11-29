8d86c5d368183d18f1b9632da3d07850abde053f
beomboo <BeomBoo@gsitm.com>
2023-11-29 17:05:34 +0900

    Initial Commit

---
+++ login/loginApp.swift
@@ -0,0 +1,17 @@
+//
+//  loginApp.swift
+//  login
+//
+//  Created by GSITM on 11/29/23.
+//
+
+import SwiftUI
+
+@main
+struct loginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
